8ddd6d7c745b2cadd0588ad98913885add0913e3
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-02 18:53:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('maintxt');
 element.innerHTML="New Value";
+var img = document.getElementById('madi');
+img.onclick=function(){
+  img.style.marginLeft="100px" ; 
+};
