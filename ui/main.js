71d42c50528ba314fb0ef4d04619af7b7f31b13a
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-02 19:07:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML="New Value";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-  marginLeft = marginLeft + 2;
+  marginLeft = marginLeft + 10;
   img.style.marginLeft= marginLeft + "px";
 }
 img.onhover=function(){
