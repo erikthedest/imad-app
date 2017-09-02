634740cd8c362af4e2603e1c595847b03a5bcbc9
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-02 16:04:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Here i am the one');
+  res.send('Here i am the one');
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile('Here i am the two');
+  res.send('Here i am the two');
 });
 
 app.get('/third', function (req, res) {
-  res.sendFile('Here i am the third');
+  res.send('Here i am the third');
 });
 
 app.get('/ui/madi.png', function (req, res) {
