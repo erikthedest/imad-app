8e813f7bfc922136ea6a14de682d6d5490261f55
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-10 13:51:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ submit.onclick=function(){
     var names=["name1","name2","name3","name4"];
     list="";
     for(var i=0;i<names.length;i++){
-        list="<li>"+names[i]+"</li>";
+        list=list+"<li>"+names[i]+"</li>";
     }
     var ul=document.getElementById('lst');
     ul.innerHTML=list;
