935c5713a627e8d3ca61c149ed6aa8d6b473feba
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-10 14:03:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ submit.onclick=function(){
             if(request.status===200){
                 var names=request.responseText;
                 names=JSON.parse(names);
-                list="";
+                var list="";
                 for(var i=0;i<names.length;i++){
                     list=list+"<li>"+names[i]+"</li>";
                 }
