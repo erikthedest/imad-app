686b77644da4227f71a8bfc97865239a08f799c7
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-15 02:01:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+var Pool = require('pg').pool;
 
 var config = {
     user:'guptaayush3108',
