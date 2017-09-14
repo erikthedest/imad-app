7ce559f60a3a6c54c690290bdead168a19405fc5
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-15 01:32:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/testyy', function (req, res) {
         if(err){
             res.status(500).send(err.toString());
         } else{
-            res.send(JSON.stringify(result,rows))
+            res.send(JSON.stringify(result,rows));
         }
     });
 });
