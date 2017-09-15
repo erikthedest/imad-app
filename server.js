1afade22879974b686a884832f27e4ab801eb663
erikthedest <31469645+erikthedest@users.noreply.github.com>
2017-09-16 00:13:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/articles/:articleName', function (req, res) {
             res.status(500).send(err.toString());
         }else{
             if(result.rows.length === 0){
-                res.status(404).send('Article not Found');
+                res.send('Article not Found');
             }else{
                 var articleData=result.rows[0];
                 res.send(createTemplate(articleData));
